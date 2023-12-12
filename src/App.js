e67797c185a93bae262b8dcfa314d98af258f013
htetpaihtun <htetpaihtun@gmail.com>
2023-12-12 23:22:32 +0630

    added dob facets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react";
+import moment from "moment";
 
 import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
@@ -34,7 +35,8 @@ const connector = new ElasticsearchAPIConnector(
       id: "CDL_AML:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyRkNThhMGFhMGQzZWU0MTU5OGRhOGMxMTQ3NDQ2ZDU4NiQ0NTAxM2Y1N2QxMDg0MzBiYWQwZjQ2MTgxOTcwNDllYg=="
     },
     apiKey: "ZGFDN05Jd0JZbHlZNm1kd1VHTkE6bEZrUzBQOGFRX3FLdGgzYkRzZHMtUQ==",
-    index: ["aml-mas", "aml-adhoc", "aml-dj"]
+    index: ["aml-mas", "aml-adhoc", "aml-dj-person"]
+    // index: ["aml-dj-person-memoenhance"]
   },
   (requestBody, requestState, queryConfig) => {
     console.log("postProcess requestBody Call", requestBody); // logging out the requestBody before sending to Elasticsearch
@@ -51,6 +53,8 @@ const connector = new ElasticsearchAPIConnector(
         fields: [
           `person.english_name.*^${wordsCount}`,
           `entity.english_name.*^${wordsCount}`,
+          `entity.name_original_script.*^${wordsCount}`,
+          `person.name_original_script.*^${wordsCount}`,
           `dataid^300`
         ]
       }
@@ -82,13 +86,111 @@ const config = {
       dj : { raw: {} },
     },
     facets: {
-      "type": { type: "value", size: 10 },
+      type: { type: "value", size: 10 },
       "person.citizenship": { type: "value", size: 10 },
       "person.gender": { type: "value", size: 10 },
+      "last_modified": {
+        type: "range",
+        ranges: [
+          {
+            from : moment().subtract(1, "days").toISOString(),
+            name : "Today"
+          },
+          {
+            from : moment().subtract(7, "days").toISOString(),
+            name : "Within a week"
+          },
+          {
+            from : moment().subtract(1, "years").toISOString(),
+            name : "Within a year"
+          },
+          {
+            to: moment().subtract(1, "years").toISOString(),
+            name: "More than a year"
+          }
+        ]
+      },
+      "person.date_of_birth": { 
+        type: "range", 
+        ranges: [
+          {
+            "to": "1700-01-01",
+            "name": "Unknown"
+          },
+          {
+            "to": "1900-01-01",
+            "name": "1900 and below"
+          },
+          {
+            "from": "1900-01-01",
+            "to": "1920-01-01",
+            "name": "1900 - 1920"
+          },
+          {
+            "from": "1920-01-01",
+            "to": "1940-01-01",
+            "name": "1920 - 1940"
+          },
+          {
+            "from": "1940-01-01",
+            "to": "1960-01-01",
+            "name": "1940 - 1960"
+          },
+          {
+            "from": "1960-01-01",
+            "to": "1980-01-01",
+            "name": "1960 - 1980"
+          },
+          {
+            "from": "1980-01-01",
+            "to": "2000-01-01",
+            "name": "1980 - 2000"
+          },
+          {
+            "from": "2000-01-01",
+            "name": "2000 and above"
+          }
+        ]
+      },
     }
   },
-  disjunctiveFacets: ["type", "person.citizenship"],
+  disjunctiveFacets: ["type", "person.citizenship", "person.gender", "last_modified", "person.date_of_birth"] 
 };
+
+export default function App() {
+  return (
+    <SearchProvider config={config}>
+      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+        {({ wasSearched }) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={ <SearchBox autocompleteSuggestions={false} />}
+                  sideContent={
+                    <React.Fragment>
+                      <Facet field={"type"} label={"type"} />
+                      <Facet field={"person.citizenship"} label={"Citizenship"} />
+                      <Facet field={"person.gender"} label={"Gender"} />
+                      <Facet field={"last_modified"} label={"Last Modified"} />
+                      <Facet field={"person.date_of_birth"} label={"Date of Birth"} />
+                    </React.Fragment>
+                  }
+                  bodyContent={
+                    <React.Fragment>
+                      <Results resultView={CustomResultView}/>
+                    </React.Fragment>
+                  }
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
+
 
 const CustomResultView = ({
   result,
@@ -129,38 +231,6 @@ const CustomResultView = ({
         </div>
       </div>
     </li>
-  );
-}
-
-export default function App() {
-  return (
-    <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={ <SearchBox autocompleteSuggestions={false} />}
-                  sideContent={
-                    <React.Fragment>
-                      <Facet field={"type"} label={"type"} />
-                      <Facet field={"person.citizenship"} label={"Citizenship"} />
-                      <Facet field={"person.gender"} label={"Gender"} />
-                    </React.Fragment>
-                  }
-                  bodyContent={
-                    <React.Fragment>
-                      <Results resultView={CustomResultView}/>
-                    </React.Fragment>
-                  }
-                />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider>
   );
 }
 
